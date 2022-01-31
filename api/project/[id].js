0a64d90447adc58fc9165029d07a1f35daf04658
Erick Ruano <erickaleruano@gmail.com>
2022-01-30 22:50:16 -0600

    Allow all headers

--- api/project/[id].js
+++ api/project/[id].js
@@ -1,21 +1,21 @@
 const api = require('@satelite/api');
 
 const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version', 'authorization', 'Authorization'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    '*'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
   }
+  return await fn(req, res)
+}
 
-  
+
 module.exports = allowCors(api.controllers.project)
